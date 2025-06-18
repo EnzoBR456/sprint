a500c0a0bd9aaa741ff621908521db6aca43c025
Enzo dos Santos <enzobezerra9@gmail.com>
2025-06-18 11:05:25 -0300

    alterações no salvamento do link da api

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,36 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
 export default function SettingsScreen() {
-  const [url, setUrl] = useState('http://localhost:3000/api');
-    useEffect(() => {
-      const loadUrl = async () => {
-        try {
-          const savedUrl = await AsyncStorage.getItem('apiUrl');
-          if (savedUrl !== null) {
-            setUrl(savedUrl);
-          } else {
-            setUrl('http://localhost:3000/api'); // valor padrão
-          }
-        } catch (error) {
-          console.error('Erro ao carregar a URL:', error);
-        }
-      };
+  const [url, setUrl] = useState('http://localhost:8080/api/readings');
 
-      loadUrl();
-    }, []);
-
-    const saveUrl = async () => {
+  useEffect(() => {
+    const loadUrl = async () => {
       try {
-        await AsyncStorage.setItem('apiUrl', url);
-        Alert.alert('Sucesso', `URL salva: ${url}`);
+        const savedUrl = await AsyncStorage.getItem('apiUrl');
+        if (savedUrl !== null) {
+          setUrl(savedUrl);
+        } else {
+          setUrl('http://localhost:8080/api/readings');
+        }
       } catch (error) {
-        console.error('Erro ao salvar a URL:', error);
-        Alert.alert('Erro', 'Não foi possível salvar a URL');
+        console.error('Erro ao carregar a URL:', error);
       }
     };
+
+    loadUrl();
+  }, []);
+
+  const saveUrl = async () => {
+    try {
+      await AsyncStorage.setItem('apiUrl', url);
+      Alert.alert('Sucesso', `URL salva: ${url}`);
+    } catch (error) {
+      console.error('Erro ao salvar a URL:', error);
+      Alert.alert('Erro', 'Não foi possível salvar a URL');
+    }
+  };
+
   return (
     <View style={{ padding: 20 }}>
       <Text>URL da API:</Text>
@@ -39,7 +40,8 @@ export default function SettingsScreen() {
         onChangeText={setUrl}
         style={{ borderWidth: 1, padding: 10, marginBottom: 20 }}
       />
-      <Button title="Salvar" onPress={() => alert(`URL salva: ${url}`)} />
+      <Button title="Salvar" onPress={saveUrl} />
     </View>
   );
 }
+
